65bcc487a2680efff1a01ad20af31cc9986ac0c1
kcmonceaux <83311450+kcmonceaux@users.noreply.github.com>
2025-07-24 19:14:31 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,15 @@
-self.addEventListener('install', e =>
-  e.waitUntil(caches.open('v1').then(c => c.addAll([
-    './index.html','./manifest.json'
-  ])))
-);
-self.addEventListener('fetch', e =>
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)))
-);
+const CACHE = 'teddy-board-cache-v1';
+const ASSETS = [
+  '.',
+  'index.html',
+  'manifest.json',
+  'sw.js',
+  'icons/icon-192.png',
+  'icons/icon-512.png'
+];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
